21efcba2094f9ca8060cdafb38552254664296c1
tsaulon <tyrone.saulon@hotmail.com>
2018-07-10 10:38:16 -0400

    Adding Group class exercise

--- objects.js
+++ objects.js
@@ -176,6 +176,17 @@ class Matrix {
 
   //    EXERCISES
 
+  /*
+   * Write a class Vec that represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it 
+   * should save to properties of the same name.
+   *
+   * Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that 
+   * has the sum or difference of the two vectors’ (this and the parameter) x and y values.
+   *
+   * Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y)
+   * from the origin (0, 0).
+   */
+
 class Vector{
     constructor(x, y){
         this.x = x;
@@ -193,9 +204,102 @@ class Vector{
     toString(){
         return `Vector { x: ${this.x}, y: ${this.y}}`;
     }
+
+    length(){
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
 }
 
 let tyrone = new Vector(1, 2);
-let ainsley = new Vector(3, 2);
+let ainsley = new Vector(3, 4);
 
 console.log(tyrone.plus(ainsley).toString());
+console.log(ainsley.length());
+
+/*
+ * The standard JavaScript environment provides another data structure called Set. Like an instance of Map, 
+ * a set holds a collection of values. Unlike Map, it does not associate other values with those—it just tracks 
+ * which values are part of the set. A value can be part of a set only once—adding it again doesn’t have any effect.
+ *
+ * Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its 
+ * constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), 
+ * delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating 
+ * whether its argument is a member of the group.
+ *
+ * Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
+ * 
+ * Give the class a static from method that takes an iterable object as argument and creates a group that contains 
+ * all the values produced by iterating over it.
+ */
+
+ class Group{
+     constructor(group = []){
+        this.group = group;         
+     }
+
+     static from(iterObj){
+         return new Group(iterObj);
+     }
+
+     add(x){
+         try{
+
+            let flag = this.has(x);
+
+            if(flag !== false && !isNaN(flag)){
+                throw new Error(`'${x}' already exists in the group!`);
+            }
+            else{
+                this.group.push(x);
+                console.log(`New member '${x}' has been added!`);
+            }
+         } catch(e){
+             console.log(`Error: ${e.message}`);
+         }
+     }
+
+     delete(x){
+        try{
+
+            let index = this.has(x);
+
+            if(index === false){
+                throw new Error(`'${x}' does not exist in the group!`);
+            } else if(!isNaN(index)){
+                this.group.splice(index, 1);
+                console.log(`'${x}' has been deleted from the group!`);
+            }
+
+        } catch(e){
+            console.log(`Error: ${e.message}`)
+        }
+     }
+
+     // Checks for existence of parameter passed and returns the index if found or false if not.
+     has(x){
+
+        let flag = false;
+
+        this.group.some((member, index) => {
+            if(member === x){
+                flag = index;
+            }
+        })
+
+        return flag;
+     }
+
+     get members(){
+         return this.group.toString();
+     }
+ }
+
+ let group = Group.from([1, 2, 3, 4]);
+
+ console.log(group.members);    // → 1,2,3,4
+ group.add(5);
+ console.log(group.members);    // → 1,2,3,4,5
+ group.delete(2);
+ console.log(group.members);    // → 1,3,4,5
+ console.log(group.has(2));     // → false
+ console.log(group.has(3));     // → 1
