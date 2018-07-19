e3df3b14c940e7d9e24160ee5e02658044ba4653
tsaulon <tyrone.saulon@hotmail.com>
2018-07-19 13:16:41 -0400

    Optional expressions '?'

--- regex.js
+++ regex.js
@@ -24,3 +24,22 @@ console.log(dateTime.test("30-jan-2003 15:20"));    //  false
 let notBinary = /[^01]/;
 console.log(notBinary.test("1100100010100110"));    //  false
 console.log(notBinary.test("1100100010200110"));    //  true
+
+
+//  Repetitions in a pattern
+//  '+' indicates potential for element to be repeated more than once.
+//  Matching one or more characters.
+//  '*' similar to '+' but allows pattern match of zero or more times. 
+
+console.log(/'\d+'/.test("'123'")); //  true
+console.log(/'\d+'/.test("''"));    //  false
+console.log(/'\d*'/.test("'123'")); //  true
+console.log(/'\d*'/.test("''"));    //  true
+
+//  '?' makes a pattern optional (occurence of 0 or 1 times.)
+//  this example checks for the 'u' character but also passes the test
+//  if it doesn't exist.
+
+let neighbor = /neighbou?r/;
+console.log(neighbor.test("neighbour"));    //  true;
+console.log(neighbor.test("neighbor")); //  true;
