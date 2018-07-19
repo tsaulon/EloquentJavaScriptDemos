c8ee948e2ed50a84fbee3df259bc0be3f377b8ed
tsaulon <tyrone.saulon@hotmail.com>
2018-07-19 14:49:30 -0400

    Adding date class examples

--- regex.js
+++ regex.js
@@ -30,7 +30,6 @@ console.log(notBinary.test("1100100010200110"));    //  true
 //  '+' indicates potential for element to be repeated more than once.
 //  Matching one or more characters.
 //  '*' similar to '+' but allows pattern match of zero or more times. 
-
 console.log(/'\d+'/.test("'123'")); //  true
 console.log(/'\d+'/.test("''"));    //  false
 console.log(/'\d*'/.test("'123'")); //  true
@@ -39,7 +38,57 @@ console.log(/'\d*'/.test("''"));    //  true
 //  '?' makes a pattern optional (occurence of 0 or 1 times.)
 //  this example checks for the 'u' character but also passes the test
 //  if it doesn't exist.
-
 let neighbor = /neighbou?r/;
 console.log(neighbor.test("neighbour"));    //  true;
 console.log(neighbor.test("neighbor")); //  true;
+
+//  Indicating number of occurences using {}
+//  {4} represents 4 occurences
+//  {1, 5} represents 1 to 5 occurences
+//  {5, } represents 5 or more occurences
+dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+console.log(dateTime.test("1-30-2003 8:45"));
+
+
+//  Grouping subexpressions
+//  Using '*' or '+' on multiple elements at a time.
+//  Parentheses is needed '()'
+//  'i' avoid case sensitive evaluations.
+let cartoonCrying = /boo+(hoo+)+/i;
+console.log(cartoonCrying.test("Boohoooohoohooo"));
+
+//  Matches and Groups
+//  Regex.exec() returns null if no match is found
+//  else returns an object with details where the match succeeded
+let match = /\d+/.exec("one two 100");
+console.log(match);         //  [ '100', index: 8, input: 'one two 100' ]
+console.log(match.index);   //  8
+
+//  Selecting part of expression surrounded by parentheses
+let quotedText = /'([^']*)'/;
+console.log(quotedText.exec(`she said 'hello'`));   //  [ '\'hello\'', 'hello', index: 9, input: 'she said \'hello\'' ]
+
+//  When a position is not matched at all, its position 
+//  in the output array will be undefined
+//  Also, when a group is matched multiple times, only the last element
+//  is captured.
+console.log(/bad(ly)?/.exec("bad"));    //  [ 'bad', undefined, index: 0, input: 'bad' ]
+console.log(/(\d)+/.exec("123"));       //  [ '123', '3', index: 0, input: '123' ]
+
+//  The Date Class
+//  Date class months start at 0 (December == 11);
+console.log(new Date());    //  print current date
+console.log(new Date(1994, 8, 7));  //  print defined date
+console.log(new Date(1994, 8, 7, 12, 59, 59, 999)); //  print defined date and time
+
+//  Representing UNIX timestamps
+console.log(new Date(2013, 11, 19).getTime());  //  get UNIX timestamp
+console.log(new Date(1387407600000));   //  get translated timestamp
+
+//  Using regex with the Date class
+function getDate(string){
+    let [_, month, day, year] = /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+    return new Date(year, month - 1, day);
+}
+
+console.log(getDate("1-30-2018"));  //  2018-01-30T05:00:00.000Z
