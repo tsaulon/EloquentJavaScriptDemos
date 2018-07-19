0e817c41a9b19f8d43463ad8db19aae8b12309cc
tsaulon <tyrone.saulon@hotmail.com>
2018-07-19 11:45:39 -0400

    Awkward implementation of writing repetitive tests

--- bugs-errors.js
+++ bugs-errors.js
@@ -1,22 +1,39 @@
 "use strict"; //  enabling strict mode
 
-function canYouSpotTheProblem(){
-    try{
-        for(i = 0; i < 10; i++){
+function canYouSpotTheProblem() {
+    try {
+        for (i = 0; i < 10; i++) {
             console.log("Happy happy.");
         }
-    } catch(e){
+    } catch (e) {
         console.log("Error 1: " + e.message);
     }
-    
+
 }
 
 canYouSpotTheProblem();
 
-try{
-    function Person(name) { this.name = name;}
+try {
+    function Person(name) { this.name = name; }
     let ainsley = Person("Ainsley");    // error here.
     console.log(name);
-} catch(e){
+} catch (e) {
     console.log("Error 2: " + e.message);
 }
+
+//  awkward implementation of writing tests
+function test(label, body) {
+    if (!body()) console.log(`Error 3 Failed: ${label}`);
+}
+
+test("Convert Latin text to uppercase", () => {
+    return "hello".toUpperCase() == "HELLO";
+})
+
+test("Convert Greek text to uppercase", () => {
+    return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+})
+
+test("don't convert case-less characters", () => {
+    return "مرحبا".toUpperCase() == "مرحبا";
+});
